d7752293feca6460478c8a0db0f20b44f304c9b3
gusbg <38957081+gusbg@users.noreply.github.com>
2018-05-29 03:56:25 -0500

    Update Wirecloud.py

--- ge_fiware/Wirecloud/Wirecloud.py
+++ ge_fiware/Wirecloud/Wirecloud.py
@@ -1,3 +1,4 @@
+from collections import OrderedDict
 import numpy as np
 import json
 import os
@@ -10,32 +11,29 @@ test = np.genfromtxt('test.txt',delimiter='\t',dtype=str)
 os.chdir('../report')
 #os.chdir('report')
 
-reporte = {
-	'GE-name' : "wirecloud",
-	'host_name' : report[15],
-	'execution_time' : test[2],
-	'test' : test[3],	
-	'date' : report[0],
-	'time' : report[1],
-	'version' : report[2],
-	'containers' : [{
-		'container_name' : report[3],
-		'container_ID' : report[4],
-		'status' : report[5]
-		}, {
-		'container_name' : report[6],
-		'container_ID' : report[7],
-		'status' : report[8]
-		},{
-		'container_name' : report[9],
-		'container_ID' : report[10],
-		'status' : report[11]
-		},{
-		'container_name' : report[12],
-		'container_ID' : report[13],
-		'status' : report[14]
-		}]
-	}
+
+reporte = OrderedDict()
+reporte['GE-name'] = "wirecloud"
+reporte['Version'] = "latest"
+reporte['Host_name'] = report[15]
+reporte['Test'] = test[len(test)-1]
+reporte['Execution_time'] = test[len(test)-2]
+reporte['Date'] = report[0]
+reporte['Time'] = report[1]
+reporte['Containers'] = OrderedDict()
+reporte['Containers']['Container_name 1'] = report[3]
+reporte['Containers']['Container_ID 1'] = report[4]
+reporte['Containers']['Status 1'] = report[5]
+reporte['Containers']['Container_name 2'] = report[6]
+reporte['Containers']['Container_ID 2'] = report[7]
+reporte['Containers']['Status 2'] = report[8]
+reporte['Containers']['Container_name 3'] = report[9]
+reporte['Containers']['Container_ID 3'] = report[10]
+reporte['Containers']['Status 3'] = report[11]
+reporte['Containers']['Container_name 4'] = report[12]
+reporte['Containers']['Container_ID 4'] = report[13]
+reporte['Containers']['Status 4'] = report[14]
+
 with open('report.json','a') as file:
 	json.dump(reporte, file, indent=4)
 
