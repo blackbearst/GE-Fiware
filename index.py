ac3edade2d835f7842e552bdc0b7f1b79a6dd169
Gustavo Torres Salinas <38081495+Gustavo-Torres-Salinas@users.noreply.github.com>
2018-06-01 00:05:00 -0500

    Update index.py

--- index.py
+++ index.py
@@ -33,13 +33,13 @@ os.system('docker-compose up -d')
 print('------------- status of containers -------------')
 os.system('docker-compose ps')
 time.sleep(5)
-os.system('cd ge_fiware/Kurento && python Kurento.py') #run KMS
+#os.system('cd ge_fiware/Kurento && python Kurento.py') #run KMS
 os.system('cd ge_fiware/Orion && python Orion.py') #run Orion
 os.system('cd ge_fiware/Wirecloud && python Wirecloud.py') #run Wirecloud
-os.system('cd ge_fiware/Wilma && python Wilma.py') #run Wilma
+#os.system('cd ge_fiware/Wilma && python Wilma.py') #run Wilma
 os.system('cd ge_fiware/AuthzForce && python authzforce.py') #run AuthzForce
-os.system('cd ge_fiware/KeyRock && python keyrock.py') #run KeyRock
-os.system('cd ge_fiware/Knowage && python knowage.py') #run Knowage
+#os.system('cd ge_fiware/KeyRock && python keyrock.py') #run KeyRock
+#os.system('cd ge_fiware/Knowage && python knowage.py') #run Knowage
 print('------------- import report mongodb -------------')
 os.system('cd ge_fiware/report && sudo docker cp report.json gefiware_fiware_report_1:/ && sudo docker exec gefiware_fiware_report_1 bash -c "mongoimport --db report_fiware --collection fiware --file report.json"')
 print('------------- remove Selenium and Mongodb -------------')
